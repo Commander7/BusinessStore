5df3121eabe8ed65d9aaa42e3b78570307e9f336
Marcello C Bustamante <marcellusvii@hotmail.com>
2024-10-01 12:12:26 +0200

    updated pages

--- components/HeartFavorite.tsx
+++ components/HeartFavorite.tsx
@@ -14,6 +14,7 @@ const HeartFavorite = ({ product, updateSignedInUser }: HeartFavoriteProps) => {
   const router = useRouter();
   const { user } = useUser();
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [loading, setLoading] = useState(false);
   const [isLiked, setIsLiked] = useState(false);
 
